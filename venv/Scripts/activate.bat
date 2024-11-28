6ae6a80c1c33c055eb1e4d53b3d311f6d4ebefa7
PanaritSa <panarit.sakunasinha@gmail.com>
2024-11-28 18:05:08 +0700

    ok

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Toto\Documents\_PLP\House\House_style_ML\venv
+set VIRTUAL_ENV=D:\Real Estate\House_Style_Project\House_style_ML\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
